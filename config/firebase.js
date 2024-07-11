b1b319fe73a1076100174ff9f930457fcffaa508
chakibo004 <chakibheraoui4@gmail.com>
2024-07-11 22:45:15 +0100

    fixed other issues

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 const { firebaseConfig } = require('./FirebaseConfig.js');
-const serviceAccount = require('./ServiceAccount.json');
+const serviceAccount = require('../ServiceAccount.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
