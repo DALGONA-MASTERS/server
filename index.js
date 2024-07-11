eea16ab38a6fd8345c04e45409752ccab23b4e72
chakibo004 <chakibheraoui4@gmail.com>
2024-07-11 22:12:41 +0100

    env update

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-  origin: 'http://localhost:3000/',
+  origin: process.env.CLIENT_URL,
   credentials: true, 
   exposedHeaders: ['jwt'] 
 };
